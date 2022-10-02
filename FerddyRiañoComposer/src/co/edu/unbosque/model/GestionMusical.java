1dc6ceae8acb5dc79447466de1b6bb5bd56d8ae3
Lenovo <Lenovo@LAPTOP-OKQJP204>
2022-10-01 23:36:18 -0500

    Commit inicial del proyecto. 1/10/22

---
+++ FerddyRiañoComposer/src/co/edu/unbosque/model/GestionMusical.java
@@ -0,0 +1,46 @@
+package co.edu.unbosque.model;
+
+import co.edu.unbosque.model.persistence.EscribirLeerFile;
+import co.edu.unbosque.model.persistence.Propiedades;
+
+public class GestionMusical {
+	
+	private String nombreCancion;
+	private GeneracionCanciones g;
+	
+		public GestionMusical() {
+			g = new GeneracionCanciones();
+			this.nombreCancion = "";
+		}
+	
+		public void nombrarCancion() {
+			g.setCancion(this.nombreCancion + "\n" + g.getCancion());
+		}
+		
+		public void gestionarArchivo() {
+			EscribirLeerFile archivo = new EscribirLeerFile();
+			archivo.escribirarchivo(getL().getCancion());
+		}
+		
+		public String gestionarPropiedades() {
+			Propiedades prop = new Propiedades();
+			prop.escribirPropiedades();
+			return "Las propiedades son: " + "\n" + prop.leerPropiedades();
+		}
+		
+		public GeneracionCanciones getL() {
+			return g;
+		}
+
+		public void setL(GeneracionCanciones l) {
+			this.g = l;
+		}
+
+		public String getNombreCancion() {
+			return nombreCancion;
+		}
+
+		public void setNombreCancion(String nombreCancion) {
+			this.nombreCancion = nombreCancion;
+		}
+}
