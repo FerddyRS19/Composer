4855ce13070b97f22f8f2400616cd3dfb91f88be
Lenovo <Lenovo@LenovoRyzen7>
2022-10-27 19:18:01 -0500

    Tiempo1

--- IsabellaHerranDanielBullaFerddyRiañoSIMON/src/co/edu/unbosque/view/PanelNumeros.java
+++ IsabellaHerranDanielBullaFerddyRiañoSIMON/src/co/edu/unbosque/view/PanelNumeros.java
@@ -2,6 +2,10 @@ package co.edu.unbosque.view;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.time.OffsetTime;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Function;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -21,24 +25,32 @@ public class PanelNumeros extends JPanel{
 	
 	public PanelNumeros() {
 		setLayout(null);
-		numero = new JLabel("Numero:");
-		numero.setBounds(20, 65, 100, 30);
-		numero1 = new JLabel("1");
-		numero1.setBounds(160, 10, 140, 140);
-		numero2 = new JLabel("2");
-		numero2.setBounds(320, 10, 140, 140);
-		numero3 = new JLabel("3");
-		numero3.setBounds(460, 10, 140, 140);
-		numero4 = new JLabel("4");
-		numero4.setBounds(600, 10, 140, 140);
-		add(numero);
-		add(numero1);
-		add(numero2);
-		add(numero3);
-		add(numero4);
+	
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			public void run() {
+				numero = new JLabel("Numero:");
+				numero.setBounds(20, 65, 100, 30);
+				numero1 = new JLabel("1");
+				numero1.setBounds(160, 10, 140, 140);
+				numero2 = new JLabel("2");
+				numero2.setBounds(320, 10, 140, 140);
+				numero3 = new JLabel("3");
+				numero3.setBounds(460, 10, 140, 140);
+				numero4 = new JLabel("4");
+				numero4.setBounds(600, 10, 140, 140);
+				add(numero);
+				add(numero1);
+				add(numero2);
+				add(numero3);
+				add(numero4);
+			}
+		}, 0, 5000);
 	}
 	
 	
 	
-	
+
+
 }
+
